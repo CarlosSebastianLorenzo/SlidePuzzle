9048c774c24d176514ac892218795874d86b0c89
CarlosSebastianLorenzo <carlossebastianlorenzo@gmail.com>
2023-03-31 00:37:50 -0300

    se creó un slide puzzle con Grid

---
+++ assets/js/index.js
@@ -0,0 +1,58 @@
+let $main = document.getElementsByTagName('main')[0];
+
+let obj = {}
+let sections = [];
+for (let i = 0; i < 3; i++) {
+    for (let j = 0; j < 3; j++) {
+        let section = {
+            gridRow: `${i+1}/${i+2}`,
+            gridColumn: `${j+1}/${j+2}`
+        };
+        sections.push(section);
+    }
+}
+sections.pop();
+sections.sort(function(a, b) {return Math.random() - 0.5});
+let i = 1
+sections.forEach(e=>{
+    let div = document.createElement('div');
+    div.classList.add('img');
+    div.classList.add('clas'+i);
+    div.style.gridRow = e.gridRow
+    div.style.gridColumn = e.gridColumn
+    $main.appendChild(div);
+    i++
+})
+let div = document.createElement('div');
+    div.setAttribute('id','empty');
+    div.style.gridRow = "3/4"
+    div.style.gridColumn = "3/4"
+    $main.appendChild(div);
+
+let $empty = document.getElementById('empty')
+$main.addEventListener('click', function(e){
+
+    let targetRowStart =  parseInt(e.target.style.gridRowStart);
+    let targetRowEnd =  parseInt(e.target.style.gridRowEnd);
+    let targetColumnStart =  parseInt(e.target.style.gridColumnStart);
+    let targetColumnEnd =  parseInt(e.target.style.gridColumnEnd);
+
+    let emptyRowStart = parseInt($empty.style.gridRowStart);
+    let emptyRowEnd = parseInt($empty.style.gridRowEnd);
+    let emptyColumnStart = parseInt($empty.style.gridColumnStart);
+    let emptyColumnEnd = parseInt($empty.style.gridColumnEnd);
+
+    if(Math.abs(targetRowStart-emptyRowStart+
+    targetRowEnd-emptyRowEnd+
+    targetColumnStart-emptyColumnStart+
+    targetColumnEnd-emptyColumnEnd) == 2){
+        e.target.style.gridRow = `${emptyRowStart}/${emptyRowEnd}`
+        e.target.style.gridColumn = `${emptyColumnStart}/${emptyColumnEnd}`
+        $empty.style.gridRow = `${targetRowStart}/${targetRowEnd}`
+        $empty.style.gridColumn = `${targetColumnStart}/${targetColumnEnd}`
+    }
+    // console.log(e.target)
+    // console.log(e)
+    // console.log(e.target.style.gridRow)
+    // this.style.gridRow = "2/3"
+});
